728202d3bda78136e68abc1033206e70adee4131
Paul Chibueze <chibuezedevloper@gmail.com>
2024-07-07 13:34:33 +0100

    update

--- src/pages/waitlist/index.jsx
+++ src/pages/waitlist/index.jsx
@@ -3,14 +3,14 @@ import "./waitlist.css";
 import Layout from "@/components/Layout";
 import { BsArrowRight } from "react-icons/bs";
 import PrimaryButton from "@/components/buttons/PrimaryButton";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Waitlist = () => {
   const [email, setEmail] = useState("");
   const [buttonText, setButtonText] = useState("Join");
   const [resultMessage, setResultMessage] = useState("");
   const [isButtonDisabled, setIsButtonDisabled] = useState(false);
-  const [showModal, setShowModal] = useState(false);
-  const [modalContent, setModalContent] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -22,30 +22,30 @@ const Waitlist = () => {
       setButtonText("Sending...");
       setIsButtonDisabled(true);
 
-      const response = await fetch("https://block-vault-server.vercel.app/api/subscribers/waitlist", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email }),
-      });
+      const response = await fetch(
+        "https://block-vault-server.vercel.app/api/subscribers/waitlist",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email }),
+        }
+      );
 
       const data = await response.json();
       if (response.ok) {
-        setModalContent("Subscription successful!");
-        setShowModal(true);
+        toast.success("Subscription successful!");
         setResultMessage("You have successfully joined the waitlist.");
         setButtonText("Success!");
       } else {
-        setModalContent("Subscription failed. Please try again later.");
-        setShowModal(true);
+        toast.error("Subscription failed. Please try again later.");
         setResultMessage(data.message);
         setButtonText("Try again");
       }
     } catch (error) {
       console.error("Error subscribing:", error);
-      setModalContent("An unexpected error occurred.");
-      setShowModal(true);
+      toast.error("An unexpected error occurred.");
       setResultMessage(error.message);
       setButtonText("Try again");
     } finally {
@@ -53,17 +53,14 @@ const Waitlist = () => {
     }
   };
 
-  const closeModal = () => {
-    setShowModal(false);
-  };
-
   return (
     <Layout>
       <div className="container-section">
         <h1>Be the first to know when we launch</h1>
         <p>
-          At BlockVault, We are looking to solve the problems of trading automation,
-          stock ML model integration, explained trading insights and future and past events aggregations.
+          At BlockVault, We are looking to solve the problems of trading
+          automation, stock ML model integration, explained trading insights and
+          future and past events aggregations.
         </p>
         <form className="join-wrapper" onSubmit={handleSubmit}>
           <input
@@ -93,33 +90,18 @@ const Waitlist = () => {
           </p>
         </div>
       </div>
-
-      {/* Modal */}
-      {showModal && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-blue-500 bg-opacity-75">
-          <div className="relative bg-white rounded-lg p-8 max-w-md mx-auto z-50">
-            <button
-              className="absolute top-0 right-0 mt-4 mr-4 text-gray-500 hover:text-gray-800"
-              onClick={closeModal}
-            >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
-            <p className="text-lg text-white">{modalContent}</p>
-          </div>
-        </div>
-      )}
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </Layout>
   );
 };
